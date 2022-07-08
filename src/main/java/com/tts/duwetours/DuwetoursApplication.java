408b1287d8941dc90cdb1cf81cd2309a80ae0072
Duwe Aynab <mfaynab88@gmail.com>
2022-07-07 20:26:00 -0400

    latest commit

---
+++ src/main/java/com/tts/duwetours/DuwetoursApplication.java
@@ -0,0 +1,13 @@
+package com.tts.duwetours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuwetoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuwetoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/duwetours/DuwetoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.duwetours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuwetoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
